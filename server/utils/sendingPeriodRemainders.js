0bc569876af3a98ade1ee445bf1172a1ba5d5453
mishab <mishab1441@gmail.com>
2025-01-30 09:50:44 +0530

    updated

--- server/utils/sendingPeriodRemainders.js
+++ server/utils/sendingPeriodRemainders.js
@@ -21,17 +21,11 @@ const twilioClient = twilio(accountSid, authToken);
         }
       }
      async function sendReminders(){
-<<<<<<< HEAD
-=======
-        const students = await Student.find();
-        console.log(students);
->>>>>>> a339ce04614246c11ebb2c613c23b078000d4489
         const now = new Date();
         const currentTime = `${now.getHours().toString().padStart(2, '0')}:${now
           .getMinutes()
           .toString()
           .padStart(2, '0')}`;
-<<<<<<< HEAD
 
           const students = await Student.find({
             'schedule.startingTime': "09:30",
@@ -40,15 +34,6 @@ const twilioClient = twilio(accountSid, authToken);
           if (students.length === 0) {
             console.log('No reminders to send at this time.');
             return;
-=======
-        for (const student of students) {
-          for (const period of student.schedule) {
-            console.log(period);
-            if (period.startingTime === currentTime) {
-              const message = `Hi ${student.name}, this is a reminder for your class on "${period.subject}" starting at ${period.startingTime}.`;
-              await sendSMS(student.phone, message);
-            }
->>>>>>> a339ce04614246c11ebb2c613c23b078000d4489
           }
           for (const student of students) {
             // Find the exact period matching the current time
